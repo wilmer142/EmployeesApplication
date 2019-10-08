7e73907a96d76bb4053ab734a1d1584e922c00c4
wilmer142 <wilmercollege@gmail.com>
2019-10-08 18:15:55 -0500

    Creating a enumeration for manage ContractTypes on factory

---
+++ BusinessLogic/Enumerations/EContractType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusinessLogic.Enumerations
+{
+    public enum EContractType : byte
+    {
+        HourlySalaryEmployee = 0,
+        MonthlySalaryEmployee = 1
+    }
+}
